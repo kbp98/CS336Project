97d506e7f9f055382813b0c0ab0f3711b73946b9
kbp98 <144631820+kbp98@users.noreply.github.com>
2024-11-25 17:36:03 -0500

    LoginSystem.sql

--- LoginSystem.sql
+++ LoginSystem.sql
@@ -1,6 +1,8 @@
 CREATE DATABASE IF NOT EXISTS `LoginSystem`;
 USE `LoginSystem`;
 
+DROP TABLE IF EXISTS `users`;
+
 CREATE TABLE users(
 `username` varchar(30) 	NOT NULL,
 `password` varchar(30) 	NOT NULL,
